34a238182c979346eba3fbf6ddb31e28c6f7a637
kimy242 <kimy242@newschool.edu>
2015-11-18 00:30:17 +0000

    msg ...

--- myfolder/random/pattern.js
+++ myfolder/random/pattern.js
@@ -10,3 +10,21 @@ var x = 4,
 x < y // true
 
 1 > 1 // false
+
+
+$(window).mousemove(function(event) {
+    var newTop = event.clientY + "px";
+    var newLeft = event.clientX + "px";
+    $(".p").css("top",newTop);
+    $(".p").css("left",newLeft);
+})
+
+$(window).keypress(function(event){
+    if (event.keyCode == 114 {
+        $("p").css("background-color","red")
+    if (event.keyCode == 103 {
+        $("p").css("background-color","lime")
+    if (event.keyCode == 98 {
+        $("p").css("background-color","blue")
+    })
+})

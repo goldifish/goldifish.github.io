091a4a3b5098b8307c116ad6202b1f42b9e47b58
kimy242 <kimy242@newschool.edu>
2015-11-19 21:53:09 +0000

    msg ...

--- myfolder/type/typepage_0main.js
+++ myfolder/type/typepage_0main.js
@@ -4,3 +4,20 @@ $('.container').hover(function() {
 	$(this).children('.hide').removeClass('turnitgray');	
 });
 
+var clicked = false;
+var x = event.clientX;
+var y = event.clientY;
+
+var purpleShape = '<div class="back"><div class="f"><div class= "purple"> </div><div class= "purple purplebottom"> </div></div><div class="f1"><div class= "purple"> </div><div class= "purple purplebottom"> </div></div></div>'
+
+$('.container').mouseenter(function(event) {
+	$('.container').append(purpleShape).css('position', 'fixed');
+	console.log('hi')
+})
+
+// $('.back').hover(function() {
+// 	$(this).children('.f').addClass('hide');	
+// }, function() {
+// 	$(this).children('.F').removeClass('hide');	
+// });
+

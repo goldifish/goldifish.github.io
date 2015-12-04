1344fb044ecbf7b943ce1d9beab50a7609551a28
kimy242 <kimy242@newschool.edu>
2015-12-04 15:37:30 +0000

    msg ...

--- myfolder/random/random1.js
+++ myfolder/random/random1.js
@@ -2,23 +2,53 @@ var ch = ["c_01.jpg", "c_02.jpg", "c_03.jpg", "c_04.jpg", "c_05.jpg", "c_06.jpg"
 
 
 for (var increment = 0; increment < 100; increment++) {
-  $(".ch").append('<div class="five"></div>');
+//   $(".ch").append('<div class="five"></div>');
 }
 
-var randomCat = ch[Math.floor(Math.random()*11)];
+// var randomCat = ch[Math.floor(Math.random()*11)];
 
-$(".ch").append("<img src='"+ randomCat + "'>");
-$(".ch").css({"background-color" : "black"})
+// $(".ch").append("<img src='"+ randomCat + "'>");
+// $(".ch").css({"background-color" : "black"})
 
 
 
-$(".random1").click(function(){
-    var randomCat= ch[Math.floor(Math.random()*11)]
-    
-    
+// $(".random1").click(function(){
+//     var randomCat= ch[Math.floor(Math.random()*11)]
+// });
     
     
 var pattern = ["p_01.jpg", "p_02.jpg", "p_03.jpg", "p_04.jpg", "p_05.jpg", "p_06.jpg", "p_07.jpg", "p_08.jpg", "p_09.jpg", "p_10.jpg"];
+
+    var randomPattern = pattern[Math.floor(Math.random()*10)];
+    var randomPhoto = ch[Math.floor(Math.random()*10)];
+    $('body').css('background-image', 'url(' + randomPattern + ')');
+    $('.ch').css('background-image', 'url(' + randomPhoto + ')'); 
+
+$('.ch').click(function() {
+    // var randomPattern = pattern[Math.floor(Math.random()*11)];
+    // var randomPhoto = ch[Math.floor(Math.random()*11)];
+    // $('body').css('background-image', 'url(' + randomPattern + ')');
+    // $('.ch').css('background-image', 'url(' + randomPhoto + ')');
+    var randomPattern = pattern[Math.floor(Math.random()*10)];
+    var randomPhoto = ch[Math.floor(Math.random()*10)];
+    $('body').css('background-image', 'url(' + randomPattern + ')');
+    $('.ch').css('background-image', 'url(' + randomPhoto + ')'); 
+});
+
+$('body').click(function() {
+    // var randomPattern = pattern[Math.floor(Math.random()*11)];
+    // var randomPhoto = ch[Math.floor(Math.random()*11)];
+    // $('body').css('background-image', 'url(' + randomPattern + ')');
+    // $('.ch').css('background-image', 'url(' + randomPhoto + ')');
+    var randomPattern = pattern[Math.floor(Math.random()*10)];
+    $('body').css('background-image', 'url(' + randomPattern + ')');
+});
+
+// $('body').hover(function() {
+//   $(this).attr('src', '/folder/image2.jpg');
+// }, function() {
+//   $(this).attr('src', '/folder/image1.jpg');
+// });
 
 
 // var randomCat = pattern[Math.floor(Math.random()*10)];
@@ -35,29 +65,18 @@ var pattern = ["p_01.jpg", "p_02.jpg", "p_03.jpg", "p_04.jpg", "p_05.jpg", "p_06
 // $(".pattern").css({"background-color" : "black"});
 // })
 
-// // $(".ch").click(function(){
-// //     var randomCat= pattern[Math.floor(Math.random()*10)]
+
+// $(".p").click(function(){
+// $(".p").css({"background" : "url("+randomCat+")"})}
+
+
+
+
+
+// $(".ch").click(function(){
+//     var randomCat= pattern[Math.floor(Math.random()*10)]
     
-// // $(".pattern").append("<img src='"+ randomCat + "'>");
-// // })
+// $(".pattern").append("<img src='"+ randomCat + "'>");
+// })
 
-// // })
-
-var randomCat = pattern[Math.floor(Math.random()*11)];
-
-$('body').css('background-image', 'url(' + randomCat + ')');
-})
-
-var pressed = false;
-
-$('body').click(function() {
-    if (pressed == false) {
-        $('body').css('background-image', 'url(' + randomCat + ')');
-        pressed = true;
-    } else {
-        $('body').css('background-image', 'url(' + randomCat + ')');
-    }
-    
-})
-
-
+// })
